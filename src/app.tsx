c9c90847b90f0be82f155d55f59fcfc0eab37530
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-10 20:48:37 +0700

    feat: profile component

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
-import { Card } from "./components/card";
+import { Profile } from "./components/profile";
 
 export function App() {
   return (
     <div>
       <header>
         <h1>Task Management</h1>
-        <img src="/vite.svg" alt="Logo" />
       </header>
 
-      <Card />
       <p className="read-the-docs">Application to manage tasks. </p>
+
+      <Profile />
     </div>
   );
 }
