31cf12c13803c33766b14ae9edb7f25f248b1c23
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-18 14:31:24 +0700

    feat: delete button

--- src/components/task-list-item.tsx
+++ src/components/task-list-item.tsx
@@ -5,6 +5,10 @@ type TaskItemProps = {
 };
 
 export function TaskListItem({ taskItem }: TaskItemProps) {
+  function deleteTask() {
+    console.log("Delete task");
+  }
+
   return (
     <div className="flex gap-2">
       {taskItem.isCompleted && <span>✅</span>}
@@ -15,6 +19,15 @@ export function TaskListItem({ taskItem }: TaskItemProps) {
         <p className="text-sm">
           <time>{taskItem.date.toDateString()}</time>
         </p>
+      </div>
+
+      <div className="flex items-center">
+        <button
+          onClick={deleteTask}
+          className="rounded bg-red-800 p-1 text-xs text-white"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
