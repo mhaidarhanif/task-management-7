4b6b673fc74a8735bc431526533d7954d58167c5
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-18 14:59:51 +0700

    feat: add task item with state

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,28 @@ import { useState } from "react";
 import { TaskList } from "./components/task-list";
 import { Task } from "./types/task";
 
-const taskItems: Task[] = [
+const initialTaskItems: Task[] = [
   { id: 1, text: "Makan malam", isCompleted: true, date: new Date() },
   { id: 2, text: "Tidur", isCompleted: false, date: new Date() },
   { id: 3, text: "Bangun", isCompleted: false, date: new Date() },
 ];
 
 export function App() {
+  const [taskItems, setTaskItems] = useState(initialTaskItems);
+
+  function addTaskItem() {
+    const newTaskItem = {
+      id: taskItems[taskItems.length - 1].id + 1,
+      text: "Tugas baru",
+      isCompleted: false,
+      date: new Date(),
+    };
+
+    const updatedTaskItems = [...taskItems, newTaskItem];
+
+    setTaskItems(updatedTaskItems);
+  }
+
   return (
     <div className="flex justify-center">
       <div className="space-y-8">
@@ -19,37 +34,20 @@ export function App() {
             </h1>
           </header>
 
-          <p className="read-the-docs">Application to manage tasks. </p>
+          <p className="read-the-docs">{taskItems.length} tasks</p>
+
+          <div>
+            <button
+              onClick={addTaskItem}
+              className="rounded bg-blue-800 p-1 text-xs text-white"
+            >
+              Add Task
+            </button>
+          </div>
 
           <TaskList taskItems={taskItems} />
         </main>
-
-        <div>
-          <ButtonCounter />
-        </div>
       </div>
-    </div>
-  );
-}
-
-function ButtonCounter() {
-  const [count, setCount] = useState(0);
-
-  function handleIncrement() {
-    const newCount = count + 1;
-    setCount(newCount);
-  }
-
-  return (
-    <div>
-      <h1>Counter</h1>
-      <p>Count: {count}</p>
-      <button
-        onClick={handleIncrement}
-        className="rounded bg-green-800 p-1 text-xs text-white"
-      >
-        Increment
-      </button>
     </div>
   );
 }
