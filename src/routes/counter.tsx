8fe40144c22c39a3aed2dcc1c2109d0abdd0c05a
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-31 22:06:16 +0700

    feat: counter example save in localStorage

--- src/routes/counter.tsx
+++ src/routes/counter.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
 export function CounterRoute() {
+  const [count, setCount] = useState(() => {
+    const storedCount = localStorage.getItem("count");
+    return storedCount ? JSON.parse(storedCount) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
   return (
     <div>
       <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
     </div>
   );
 }
